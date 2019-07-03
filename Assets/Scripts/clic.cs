ac5982c80a9f3c8d42907fb3ea48ac3f7172058b
Mihail <scrayerful@gmail.com>
2019-07-04 00:08:03 +0500

    Gameplay updates
    Added trees reaction

---
+++ Assets/Scripts/clic.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clic : MonoBehaviour
+{
+
+    public float shakeSpeed = 1f;
+    bool alive = true;
+    public int health = 5;
+    int count = 0;
+    
+
+    void Shake(float speed)
+    {
+        var rotation = transform.eulerAngles;
+        rotation.x += speed*1000;
+        transform.eulerAngles = rotation;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        var rotation = transform.eulerAngles;
+
+        if (alive == false)
+        {
+            Shake(shakeSpeed);
+            count++;
+        }
+        if (count > 10)
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<scip>().enabled = false;
+        if (health > 0)
+        {
+            health--;
+            Shake(shakeSpeed);
+        }
+        else alive = false;
+    }
+}
---
+++ Assets/Scripts/play.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour
+{
+
+    public int firewood = 5;
+    public int cuttedTrees = 0;
+    public int firePower = 10;
+    public int AxeState = 10;
+    public int BeardState = 0;
+    public int temperature = 10;
+    public int time = 1;
+    public int magicTempConst = 100;
+    public int magicTimeConst = 10;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+        if (time % ((magicTempConst - temperature) * magicTimeConst) == 0)
+        {
+            time = 1;
+            firePower--;
+            print("Fire Power: " + firePower);
+        }
+        else if (firePower < 1)
+        {
+            print("GameOver");
+            
+        }
+    }
+}
